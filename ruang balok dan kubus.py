77fd857bcada11e820121aa842c2b8305519dfc3
ameliazlfa <113221661+ameliazlfa@users.noreply.github.com>
2022-09-15 21:44:41 +0700

    Add files via upload

---
+++ ruang balok dan kubus.py
@@ -0,0 +1,14 @@
+#MENGHITUNG VOLUME BALOK
+p = float(input("Masukkan Nilai panjang = "))
+l = float(input("Masukkan Nilai lebar = "))
+t = float(input("Masukkan Nilai Tinggi = "))
+v = p*l*t
+print ("Volume Balok = %0.2f" %v)
+
+#MENGHITUNG VOLUME KUBUS
+sisi = float(input("Masukkan sisi : "))
+VolumeKubus = sisi * sisi * sisi
+print("Volume Kubus =",VolumeKubus)
+input('Press ENTER to exit')
+
+
